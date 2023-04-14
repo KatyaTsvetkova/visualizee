6ddf43448ce6366beb749f932e8a6b93c0934498
KatyaTsvetkova <87665385+KatyaTsvetkova@users.noreply.github.com>
2023-04-14 22:08:19 +0300

    Adding spinner, pin and MasonryLayout

--- visualizee_frontend/src/utils/data.js
+++ visualizee_frontend/src/utils/data.js
@@ -2,3 +2,52 @@ export const userQuery = (userId) => {
     const query = `*[_type =="user" && _id =='${userId}']`;
     return query;
 }
+
+export const searchQuery = (searachTerm) => {
+    const query = `*[_type== "pin"&& titel match '${searachTerm}*'|| category match '${searachTerm}*' || about match '${searachTerm}*']{
+       image{
+        asset -> {
+            url
+        }
+       } ,
+       _id,
+       destination,
+       postedBy ->{
+        _id,
+        userName,
+        image
+       },
+       save[]{
+        _key,
+        postedBy->{
+            _id,
+            userName,
+            image
+        },
+       },
+    }`
+    return query;
+}
+export const feedQuery = `*[_type =='pin'] | order(_createAt desc){
+    image{
+        asset -> {
+            url
+        }
+       } ,
+       _id,
+       destination,
+       postedBy ->{
+        _id,
+        userName,
+        image
+       },
+       save[]{
+        _key,
+        postedBy->{
+            _id,
+            userName,
+            image
+        },
+       }, 
+}`
+
